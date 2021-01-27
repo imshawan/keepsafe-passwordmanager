e93cdb9d6d110e965cdde22f6d953d4922b8ab26
Shawan Mandal <70664409+imshawan@users.noreply.github.com>
2021-01-27 20:37:42 +0530

    updates

--- handle_db.py
+++ handle_db.py
@@ -1,22 +1,57 @@
 import sqlite3, os
 
 
-def create_DB():
+def create_DB(category):
+
     '''This function creates a database for KeepSafe Application'''
-    getConnect = sqlite3.connect('keepsafe.db') # Database name
-    getCurser = getConnect.cursor()
-	# Create table
-    getCurser.execute('''CREATE TABLE KeepSafe(category text, username text, password text)''')
-    getConnect.commit()
-    getConnect.close()
+    get_DB = sqlite3.connect('keepsafe.db') # Database name
+
+    #IF empty table, then just create the database file and return
+    if category == "":
+        return
+    else:
+        getCurser = get_DB.cursor()
+            # Create table
+        getCurser.execute(f'CREATE TABLE {category}(username text, password text)')
+        get_DB.commit()
+        get_DB.close()
 
 def addElements(category, username, passwd):
-    if(not(path.exists("keepsafe.db"))):
+    if(not(os.path.exists('keepsafe.db'))):
         create_DB()
-    pass
+    get_DB = sqlite3.connect('keepsafe.db')
+    getCurser = get_DB.cursor()
+    getCurser.execute(f"INSERT INTO {category} VALUES ('%s','%s')"%(username,passwd))
+    
+    get_DB.commit()
+    get_DB.close()
 
 def modifyElements(category, username, passwd):
-    pass
+    if(not(os.path.exists('keepsafe.db'))):
+        raise RuntimeError('No Databases Found!')
+    else:
+        get_DB = sqlite3.connect('keepsafe.db')
+        getCurser = get_DB.cursor()
+        getCurser.execute(f'UPDATE {category} SET password={passwd} WHERE username={username}')
+        get_DB.commit()
+        get_DB.close()
 
 def delElements(category, username, passwd):
-    pass
+    if(not(os.path.exists('keepsafe.db'))):
+        raise RuntimeError('No Databases Found!')
+    else:
+        get_DB = sqlite3.connect('keepsafe.db')
+        getCurser = get_DB.cursor()
+        getCurser.execute('DELETE FROM {category} WHERE username={username}')
+        get_DB.commit()
+        get_DB.close()
+
+def getElements(category):
+    if(not(os.path.exists('keepsafe.db'))):
+        raise RuntimeError('No Databases Found!')
+    else:
+        get_DB = sqlite3.connect('keepsafe.db')
+        getCurser = get_DB.cursor()
+        for row in getCurser.execute(f'SELECT * FROM {category}'):
+            print(row)
+        get_DB.close()
