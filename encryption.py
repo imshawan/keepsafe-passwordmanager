64fdeb50adac40179c4a66b80b5f8d14c669f5db
Shawan Mandal <70664409+imshawan@users.noreply.github.com>
2021-02-04 20:35:34 +0530

    encryption and decryption

--- encryption.py
+++ encryption.py
@@ -1,3 +1,12 @@
+''' This module handles all the encryption and decryption related settings for KeepSafe - Password Manager
+    Author: Shawan Mandal
+    
+    MIT License, see LICENSE for more details.
+    Copyright (c) 2021 Shawan Mandal
+'''
+
+
+
 import base64, os
 from cryptography.fernet import Fernet, InvalidToken
 from cryptography.hazmat.backends import default_backend
@@ -7,6 +16,7 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 
 def encryptData(data, passwd):
+    '''Takes arguments as "DATA" and "PASSWORD", performs encryption and returns result'''
     password = passwd.encode()  # Convert to type bytes
     salt = b'|\xd8\x99M\xc0C\xee->o\xf8\x90w\xd1\xc50'
     kdf = PBKDF2HMAC(
@@ -23,6 +33,7 @@ def encryptData(data, passwd):
 
 
 def decryptData(data, decryptKey):
+    '''Takes arguments as "DATA" and "PASSWORD", performs decryption and returns result'''
     password_provided = decryptKey
     password = password_provided.encode()  # Convert to type bytes
     salt = b'|\xd8\x99M\xc0C\xee->o\xf8\x90w\xd1\xc50'
